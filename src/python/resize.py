8826980881ce58f46dddb8eb34e47eb1feb070d9
genghis88 <kumar.bharath1988@gmail.com>
2015-08-24 17:55:48 -0400

    Resize script

---
+++ src/python/resize.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from PIL import Image
+
+trainingLabels = pd.read_csv('./data/trainLabels.csv')
+#print trainingLabels
+
+images = []
+for index, row in trainingLabels.iterrows():
+  #print str(row['ID']) + ' ' + row['Class']
+  fileName = './data/train/' + str(row['ID']) + '.Bmp'
+  im = Image.open(fileName)
+  im = im.resize((20, 20), Image.ANTIALIAS)
+  images.append(im)
+  newFileName = './data/train/resized/' + str(row['ID']) + '.bmp'
+  im.save(newFileName)
+  #pixels = im.load()
+  #images.append()
+  #break
