83b08143645f87384ddf8915a0ce8df0b036734b
unknown <kprabhu@.corp.ebay.com>
2016-03-10 15:47:42 -0800

    Updated network

--- src/neural_network/predict_lasagne.py
+++ src/neural_network/predict_lasagne.py
@@ -21,7 +21,7 @@ def writeToFile(files, y, predictionsFile):
 
 files = [ f for f in listdir(inputDir) if isfile(join(inputDir,f)) ]
 
-files = map(lambda fileNumber: str(fileNumber) + '.png', sorted(map(lambda name: int(name[:name.index('.png')]), files)))
+files = list(map(lambda fileNumber: str(fileNumber) + '.png', sorted(map(lambda name: int(name[:name.index('.png')]), files))))
 
 num_samples = len(files)
 imageSize = 1024
@@ -41,5 +41,5 @@ xTest = xTest.reshape(xTest.shape[0], 1, 32, 32).astype('float32')
 model = open(modelFile,'rb')
 net = pickle.load(model)
 y = net.predict(xTest)
-print y
+print(y)
 writeToFile(files,y,predictionsFile)
--- src/neural_network/train_lasagne.py
+++ src/neural_network/train_lasagne.py
@@ -15,18 +15,20 @@ pickleFile = sys.argv[3]
 
 trainingLabels = pd.read_csv(labelsFile)
 
+#imageSize = 400
 imageSize = 1024
-xTrain = np.zeros((trainingLabels.shape[0], imageSize))
+xTrain = np.zeros((trainingLabels.shape[0], imageSize), dtype='float32')
 for index, row in trainingLabels.iterrows():
   fileName = join(normalizedDataDirectory, str(row['ID']) + '.png')
   im = Image.open(fileName)
   xTrain[index, :] = np.reshape(im, (1, imageSize))
 
-y = map(ord, trainingLabels['Class'])
+y = list(map(ord, trainingLabels['Class']))
 
 xTrain /= xTrain.std(axis = None)
 xTrain -= xTrain.mean()
 
+#xTrain = xTrain.reshape(xTrain.shape[0], 1, 20, 20).astype('float32')
 xTrain = xTrain.reshape(xTrain.shape[0], 1, 32, 32).astype('float32')
 
 b = np.zeros((2, 3), dtype='float32')
@@ -35,66 +37,125 @@ b[1, 1] = 1
 
 b = b.flatten()
 
-inputLayer = layers.InputLayer(shape=(None, 1, 32, 32))
+'''inputLayer = layers.InputLayer(shape=(None, 1, 20, 20))
+
+loc1Layer = layers.Conv2DLayer(inputLayer, num_filters=20, filter_size=(3,3))
+loc2Layer = layers.MaxPool2DLayer(loc1Layer, pool_size=(2,2))
+loc3Layer = layers.Conv2DLayer(loc2Layer, num_filters=40, filter_size=(2,2))
+loc4Layer = layers.MaxPool2DLayer(loc3Layer, pool_size=(2,2))
+loc5Layer = layers.Conv2DLayer(loc4Layer, num_filters=80, filter_size=(3,3))
+loc8Layer = layers.DenseLayer(loc5Layer, num_units=320, nonlinearity=elu)
+loc9Layer = layers.DenseLayer(loc8Layer, num_units=160, nonlinearity=tanh)
+loc10Layer = layers.DenseLayer(loc9Layer, num_units=80, nonlinearity=tanh)
+locOutLayer = layers.DenseLayer(loc10Layer, num_units=6, nonlinearity=identity)
+
+transformLayer = layers.TransformerLayer(inputLayer, locOutLayer, downsample_factor=1.0)
+poolTransformLayer = layers.Upscale2DLayer(transformLayer, scale_factor=2)
+
+print('Transformer layer output shape ', poolTransformLayer.output_shape)
+
+conv1Layer = layers.Conv2DLayer(poolTransformLayer, num_filters=40, filter_size=(3,3))
+pool1Layer = layers.MaxPool2DLayer(conv1Layer, pool_size=(2,2))
+dropout1Layer = layers.DropoutLayer(pool1Layer, p=0.2)
+
+conv2Layer = layers.Conv2DLayer(dropout1Layer, num_filters=80, filter_size=(2,2))
+pool2Layer = layers.MaxPool2DLayer(conv2Layer, pool_size=(2,2))
+#pool2Layer = layers.Pool2DLayer(conv2Layer, pool_size=(2,2), mode='average_inc_pad')
+#dropout2Layer = layers.DropoutLayer(pool2Layer, p=0.2)
+
+conv3Layer = layers.Conv2DLayer(pool2Layer, num_filters=160, filter_size=(2,2))
+pool3Layer = layers.MaxPool2DLayer(conv3Layer, pool_size=(2,2))
+dropout3Layer = layers.DropoutLayer(pool3Layer, p=0.2)
+
+conv4Layer = layers.Conv2DLayer(dropout3Layer, num_filters=320, filter_size=(3,3))
+
+hidden3Layer = layers.DenseLayer(conv4Layer, num_units=1280, nonlinearity=elu)
+hidden4Layer = layers.DenseLayer(hidden3Layer, num_units=640, nonlinearity=tanh)
+hidden5Layer = layers.DenseLayer(hidden4Layer, num_units=320, nonlinearity=elu)
+hidden6Layer = layers.DenseLayer(hidden5Layer, num_units=160, nonlinearity=tanh)
+#hidden7Layer = layers.DenseLayer(hidden6Layer, num_units=80, nonlinearity=tanh)
+outputLayer = layers.DenseLayer(hidden6Layer, num_units=62, nonlinearity=softmax)'''
 
 #Localization network
-loc1Layer = layers.Conv2DLayer(inputLayer, num_filters=32, filter_size=(5,5))
+inputLayer = layers.InputLayer(shape=(None, 1, 32, 32))
+loc1Layer = layers.Conv2DLayer(inputLayer, num_filters=32, filter_size=(3,3))
 loc2Layer = layers.MaxPool2DLayer(loc1Layer, pool_size=(2,2))
-loc3Layer = layers.Conv2DLayer(loc2Layer, num_filters=64, filter_size=(5,5))
+loc3Layer = layers.Conv2DLayer(loc2Layer, num_filters=64, filter_size=(2,2))
 loc4Layer = layers.MaxPool2DLayer(loc3Layer, pool_size=(2,2))
-loc5Layer = layers.Conv2DLayer(loc4Layer, num_filters=128, filter_size=(4,4))
-loc6Layer = layers.DenseLayer(loc5Layer, num_units=512, nonlinearity=rectify)
-#loc7Layer = layers.DenseLayer(loc6Layer, num_units=256, nonlinearity=tanh)
-#loc8Layer = layers.DenseLayer(loc7Layer, num_units=128, nonlinearity=tanh)
-#loc9Layer = layers.DenseLayer(loc8Layer, num_units=64, nonlinearity=elu)
-locOutLayer = layers.DenseLayer(loc6Layer, num_units=6, nonlinearity=identity)
+loc5Layer = layers.Conv2DLayer(loc4Layer, num_filters=128, filter_size=(2,2))
+#loc5Layer = layers.Conv2DLayer(loc4Layer, num_filters=128, filter_size=(5,5))
+loc6Layer = layers.MaxPool2DLayer(loc5Layer, pool_size=(2,2))
+loc7Layer = layers.Conv2DLayer(loc6Layer, num_filters=256, filter_size=(2,2))
+loc8Layer = layers.DenseLayer(loc7Layer, num_units=1024, nonlinearity=elu)
+loc9Layer = layers.DenseLayer(loc8Layer, num_units=512, nonlinearity=tanh)
+loc10Layer = layers.DenseLayer(loc9Layer, num_units=256, nonlinearity=elu)
+loc11Layer = layers.DenseLayer(loc10Layer, num_units=128, nonlinearity=tanh)
+locOutLayer = layers.DenseLayer(loc10Layer, num_units=6, nonlinearity=identity)
 
 #Transformer network
 transformLayer = layers.TransformerLayer(inputLayer, locOutLayer, downsample_factor=1.0)
-#transformLayer = layers.TransformerLayer(inputLayer, locOutLayer, downsample_factor=2.0)
-print 'Transformer layer output shape ', transformLayer.output_shape
+#transformLayer = layers.TransformerLayer(inputLayer, locOutLayer, downsample_factor=1.0)
+#poolTransformLayer = layers.Upscale2DLayer(transformLayer, scale_factor=2)
+print('Transformer layer output shape ', transformLayer.output_shape)
 
 #Classification network
 conv1Layer = layers.Conv2DLayer(transformLayer, num_filters=32, filter_size=(3,3))
-pool1Layer = layers.MaxPool2DLayer(conv1Layer, pool_size=(2,2))  
+pool1Layer = layers.MaxPool2DLayer(conv1Layer, pool_size=(2,2))
 dropout1Layer = layers.DropoutLayer(pool1Layer, p=0.2)
 
 conv2Layer = layers.Conv2DLayer(dropout1Layer, num_filters=64, filter_size=(4,4))
-pool2Layer = layers.MaxPool2DLayer(conv2Layer, pool_size=(2,2))  
+pool2Layer = layers.MaxPool2DLayer(conv2Layer, pool_size=(2,2))
 dropout2Layer = layers.DropoutLayer(pool2Layer, p=0.2)
 
 conv3Layer = layers.Conv2DLayer(dropout2Layer, num_filters=128, filter_size=(3,3))
-#pool3Layer = layers.MaxPool2DLayer(conv3Layer, pool_size=(2,2))  
+#pool3Layer = layers.MaxPool2DLayer(conv3Layer, pool_size=(2,2))
 #dropout3Layer = layers.DropoutLayer(pool3Layer, p=0.2)
 
 #conv4Layer = layers.Conv2DLayer(dropout3Layer, num_filters=256, filter_size=(2,2))
 
-hidden3Layer = layers.DenseLayer(conv3Layer, num_units=2048, nonlinearity=tanh)
+hidden3Layer = layers.DenseLayer(conv3Layer, num_units=2048, nonlinearity=elu)
 hidden4Layer = layers.DenseLayer(hidden3Layer, num_units=1024, nonlinearity=tanh)
-hidden5Layer = layers.DenseLayer(hidden4Layer, num_units=512, nonlinearity=tanh)
+hidden5Layer = layers.DenseLayer(hidden4Layer, num_units=512, nonlinearity=elu)
 hidden6Layer = layers.DenseLayer(hidden5Layer, num_units=256, nonlinearity=tanh)
 hidden7Layer = layers.DenseLayer(hidden6Layer, num_units=128, nonlinearity=tanh)
 outputLayer = layers.DenseLayer(hidden7Layer, num_units=62, nonlinearity=softmax)
-
-'''conv1Layer = layers.Conv2DLayer(transformLayer, num_filters=32, filter_size=(3,3))
-pool1Layer = layers.MaxPool2DLayer(conv1Layer, pool_size=(2,2))  
+#hidden7Layer = layers.DenseLayer(hidden6Layer, num_units=124, nonlinearity=softmax)
+#print(hidden7Layer.output_shape)
+#reshape1Layer = layers.ReshapeLayer(hidden7Layer, shape=(1, 1, 124))
+#outputLayer = layers.MaxPool1DLayer(reshape1Layer, pool_size=2)
+'''conv1Layer = layers.Conv2DLayer(poolTransformLayer, num_filters=32, filter_size=(3,3))
+pool1Layer = layers.MaxPool2DLayer(conv1Layer, pool_size=(2,2))
 dropout1Layer = layers.DropoutLayer(pool1Layer, p=0.2)
 
-conv2Layer = layers.Conv2DLayer(dropout1Layer, num_filters=64, filter_size=(2,2))
-pool2Layer = layers.MaxPool2DLayer(conv2Layer, pool_size=(2,2))  
+conv2Layer = layers.Conv2DLayer(dropout1Layer, num_filters=64, filter_size=(4,4))
+pool2Layer = layers.MaxPool2DLayer(conv2Layer, pool_size=(2,2))
+#pool2Layer = layers.Pool2DLayer(conv2Layer, pool_size=(2,2), mode='average_inc_pad')
 dropout2Layer = layers.DropoutLayer(pool2Layer, p=0.2)
 
-conv3Layer = layers.Conv2DLayer(dropout2Layer, num_filters=128, filter_size=(2,2))
+conv3Layer = layers.Conv2DLayer(dropout2Layer, num_filters=128, filter_size=(3,3))
+pool3Layer = layers.MaxPool2DLayer(conv3Layer, pool_size=(2,2))
+#pool3Layer = layers.Pool2DLayer(conv3Layer, pool_size=(2,2), mode='average_inc_pad')
+dropout3Layer = layers.DropoutLayer(pool3Layer, p=0.2)
 
-hidden5Layer = layers.DenseLayer(conv3Layer, num_units=512, nonlinearity=tanh)
-hidden6Layer = layers.DenseLayer(hidden5Layer, num_units=256, nonlinearity=tanh)
+conv4Layer = layers.Conv2DLayer(dropout3Layer, num_filters=256, filter_size=(3,3))
+#pool4Layer = layers.MaxPool2DLayer(conv4Layer, pool_size=(2,2))
+#dropout4Layer = layers.DropoutLayer(pool4Layer, p=0.2)
+
+#conv5Layer = layers.Conv2DLayer(dropout4Layer, num_filters=1024, filter_size=(2,2))
+
+#hidden1Layer = layers.DenseLayer(conv4Layer, num_units=8192, nonlinearity=elu)
+hidden2Layer = layers.DenseLayer(conv4Layer, num_units=4096, nonlinearity=rectify)
+hidden3Layer = layers.DenseLayer(hidden2Layer, num_units=2048, nonlinearity=tanh)
+hidden4Layer = layers.DenseLayer(hidden3Layer, num_units=1024, nonlinearity=elu)
+hidden5Layer = layers.DenseLayer(hidden4Layer, num_units=512, nonlinearity=tanh)
+hidden6Layer = layers.DenseLayer(hidden5Layer, num_units=256, nonlinearity=elu)
 hidden7Layer = layers.DenseLayer(hidden6Layer, num_units=128, nonlinearity=tanh)
 outputLayer = layers.DenseLayer(hidden7Layer, num_units=62, nonlinearity=softmax)'''
 
 net = NeuralNet(
   layers = outputLayer,
   update_learning_rate = 0.01,
-  update_momentum = 0.9,
+  update_momentum = 0.90,
   
   batch_iterator_train = BatchIterator(batch_size = 100),
   batch_iterator_test = BatchIterator(batch_size = 100),
@@ -102,7 +163,7 @@ net = NeuralNet(
   use_label_encoder = True,
   #use_label_encoder = False,
   regression = False,
-  max_epochs = 100,
+  max_epochs = 200,
   verbose = 1
 )
 
